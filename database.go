e8234ad1152ed10e21cdb418f9cdaa03c31d88c0
SimaRangamiz <152434837+SimaRangamiz@users.noreply.github.com>
2026-07-16 12:07:46 +0330

    integrate MongoDB database

---
+++ database.go
@@ -0,0 +1,32 @@
+package main
+
+import ("fmt"
+        "context"
+		"log"
+		"time"
+		"go.mongodb.org/mongo-driver/mongo"
+		"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Collection
+
+func Connect_DB() {
+	mongo_addres := "mongodb://localhost:27017"
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongo_addres))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB = client.Database("tag_extractor").Collection("articles")
+	
+	fmt.Println("Successfully connected to MongoDB!")
+	
+}
